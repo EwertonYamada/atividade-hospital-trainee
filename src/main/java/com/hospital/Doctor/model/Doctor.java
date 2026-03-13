40a6f3f13ab46a927fd0bd179a27da6dc62c1a1c
Deniel132 <danielseron195@gmail.com>
2026-03-13 14:54:00 -0300

    Requisito 2 completo

--- src/main/java/com/hospital/Doctor/model/Doctor.java
+++ src/main/java/com/hospital/Doctor/model/Doctor.java
@@ -1,10 +1,16 @@
 package com.hospital.Doctor.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.hospital.Doctor.dto.DoctorRequest;
+import com.hospital.admission.model.Admission;
 import com.hospital.ward.enums.Specialty;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "doctor")
@@ -27,5 +33,15 @@ public class Doctor {
     @Column(name = "specialty")
     @Enumerated(EnumType.STRING)
     private Specialty specialty;
+
+    @ManyToMany(mappedBy = "doctors")
+    @JsonIgnore
+    private List<Admission> admissions = new ArrayList<>();
+
+    public Doctor(DoctorRequest doctorRequest) {
+        this.name = doctorRequest.name();
+        this.crm = doctorRequest.crm();
+        this.specialty = doctorRequest.specialty();
+    }
 
 }
